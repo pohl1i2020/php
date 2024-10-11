b0cff99564817b24a7e78875460ce9facf83b161
pohl1i2020 <102365236+pohl1i2020@users.noreply.github.com>
2024-10-11 11:40:43 +0200

    Add files via upload

---
+++ k.smolnik5i2/tab.php
@@ -0,0 +1,19 @@
+<?php
+
+$tab = array();
+$suma = 0;
+for($i=0; $i<5; $i++)
+{
+    $tab[$i]=rand(1,10);
+
+    echo $tab[$i].".";
+    $suma = $suma + $tab[i];
+
+}
+
+
+echo "<br> suma elementów wynosi: ", $suma;
+
+
+?> 
+
---
+++ k.smolnik5i2/zad1.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = rand(1,10);
+$b = rand(1,10);
+
+if($a % $b == 0){
+    echo "liczba jest podzielna"; 
+}
+    
+else {
+    echo"liczba jest nie podzielna";
+};
+
+
+
+?>
